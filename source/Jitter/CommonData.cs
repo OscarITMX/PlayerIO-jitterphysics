16904381835f3a00b3d9d222f12adf8317a67e2c
OscarITMX <OscarITMX@gmail.com>
2018-11-17 15:32:46 -0600

    Initial commit

---
+++ source/Jitter/CommonData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Jitter2D
+{
+    public class CommonData
+    {
+        public int RigidBody_instanceCount = 0;
+        public int Constraint_instanceCount = 0;
+    }
+}
